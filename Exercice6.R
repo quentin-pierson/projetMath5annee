6cf3d1a86f9935a8d8854be659eb61f9cf2a9f6f
pperezdev <pierrick.perez@free.fr>
2022-09-12 18:49:45 +0200

    add exo 6

--- Exercice6.R
+++ Exercice6.R
@@ -4,8 +4,8 @@ theme_set(
     theme(legend.position = "top")
 )
 
-df <- data.frame(ann=c(4, 3, 5,22, 3, 4, 6, 21, 2, 4, 6, 24, 3, 4, 7, 26),
-                 Trimestre=c(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16))
+df <- data.frame(ann=c(172, 298, 611, 122, 254, 414, 795, 198, 300, 472, 903, 265, 466, 568, 1115, 290, 352, 624, 1274, 303),
+                 Trimestre=c(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20))
 head(df, 4)
 
 p <- ggplot(data = df, aes(x = Trimestre, y = ann, group = 1)) 
