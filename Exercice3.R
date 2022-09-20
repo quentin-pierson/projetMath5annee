8e64e87fe2009aafc6d30fc6cbd37a5a7419ee27
quentin-pierson <quentinprs62@gmail.com>
2022-09-20 18:18:48 +0200

    add function to exercice 3

--- Exercice3.R
+++ Exercice3.R
@@ -5,44 +5,37 @@ A = rbind(c(-18, 13, -4, 4), c(2, 19, -4, 12),
 A_svd = svd(A)
 A_svd
 
-"----------"
-print("V")
+svd_own <- function(A){
+  print("U")
+  AAT <- A %*% t(A)
+  AAT.e <- eigen(AAT)
+  u.mat <- AAT.e$vectors
+  u.mat[,2:4] <- u.mat[,2:4] * -1
+  
+  print(u.mat)
+  
+  print("V")
+  ATA <- t(A) %*% A
+  
+  ATA.e <- eigen(ATA)
+  v.mat <- ATA.e$vectors
+  
+  v.mat[,2:4] <- v.mat[,2:4] * -1
+  
+  print(v.mat)
+}
 
-ATA <- t(A) %*% A
-ATA
+#svd_own(A)
 
-ATA.e <- eigen(ATA)
-v.mat <- ATA.e$vectors
-v.mat
-
-
-v.mat[,1:2] <- v.mat[,1:2] * -1
-v.mat
-
-print("U")
-AAT <- A %*% t(A)
-AAT
-
-AAT.e <- eigen(AAT)
-u.mat <- AAT.e$vectors
-u.mat
-
-r <- sqrt(ATA.e$values)
-r <- r * diag(length(r))[,1:3]
-r
-
-svd.matrix <- u.mat %*% r %*% t(v.mat)
-svd.matrix
-
-
+svd_own(B)
 "------------------"
 
-https://rpubs.com/aaronsc32/singular-value-decomposition-r
+# https://rpubs.com/aaronsc32/singular-value-decomposition-r
 
 
 "-----------"
 
-B = rbind(c(-6, -8, 4, 5, 4), c(2, 7, -5, -6, 4),
+B = rbind(c(-6, -8, -4, 5, -4), c(2, 7, -5, -6, 4),
           c(0, -1, -8, 2, 2) ,c(-1, -2, 4, 4, -8))
 
 B
