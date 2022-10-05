630311782c51eaba78151a9bd32d5f21964ff4dc
antdbs <antoinedubois14@gmail.com>
2022-10-06 00:41:23 +0200

    add corr matrix

--- Exercice8.R
+++ Exercice8.R
@@ -1,3 +1,5 @@
+install.packages("ggcorrplot")                      # Install ggcorrplot package
+library("ggcorrplot")                               # Load ggcorrplot
 
 dataframe <- read.csv("donnees/base_conso.csv", header = TRUE, sep = ";") # 
 #
@@ -5,7 +7,32 @@ dataframe <- read.csv("donnees/base_conso.csv", header = TRUE, sep = ";") #
 #View(dataframe)
 
 #dataframe[is.na(dataframe)] <- 0
-dataframe <- as.numeric(dataframe) 
+#dataframe[x] <- as.numeric(dataframe[x]) 
 
-data_coor <- cor(dataframe)
-data_coor
+sapply(dataframe, class) 
+
+cols <- names(dataframe) %in% c('individus')
+
+df <- dataframe[!cols]
+
+#dataframe[is.na(dataframe)] <- 0
+
+#data$x1 <- as.numeric(as.character(data$x1))
+
+#data_coor <- round(cor(dataframe),2)
+data_coor <- round(cor(df),2)
+#data_coor
+
+corr_plot <- ggcorrplot(
+  data_coor,
+  method = "square",
+  type="full",
+  hc.order=TRUE,
+  lab=TRUE,
+  lab_size=3,
+  colors=c("red", "white", "red"),
+  title="Matrice de corrélation",
+  ggtheme=theme_bw
+)
+
+corr_plot
